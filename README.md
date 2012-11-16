d7e41ed8adb44d46fb992699193bf4e16701f5ed
Danny Lawrence <daniel@danielscottlawrence.com>
2012-11-17 01:28:21 +1100

    updated settings to move config away from code

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ dansysadm.com
 =============
 
 Website for dansysadm.com
+
+The website uses markdown and some css to build the html pages.
---
+++ settings.py
@@ -0,0 +1,17 @@
+# The path to the directory that contains all the source files that are
+# used to create the webiste.
+# This includes markdown files and css files.
+input_dir = "src"
+
+# The directory that the src will be compiled into, this might not be the end
+# point. However I compile it into a temp directory then ship it over to my 
+# servers.
+output_dir = "web"
+
+# Where the header file is, the header file should be html that is insesrted
+# before every generated page.
+header_file = "theme/header"
+
+# Where the footer file is, the footer file should be html that is insesrted
+# after every generated page.
+footer_file = "theme/footer"
